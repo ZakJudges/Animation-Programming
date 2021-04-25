9910d6032d63829908a5b839af1252c4563c1bce
ZakJudges <32545391+ZakJudges@users.noreply.github.com>
2021-04-25 18:43:35 +0100

    Added additive animations

--- Source/BlendController.cpp
+++ Source/BlendController.cpp
@@ -43,3 +43,44 @@ void BlendController::Blend(Pose& pose, Pose& a, Pose& b, float t, int blendRoot
 		pose.SetLocalTransform(i, Mix(a.GetLocalTransform(i), b.GetLocalTransform(i), t));
 	}
 }
+
+//	Not to be called during run-time.
+Pose BlendController::MakeAdditivePose(Skeleton& skeleton, Clip& clip)
+{
+	//	Getting rest pose makes the result the correct size.
+	Pose result = skeleton.GetRestPose();
+
+	//	Set the pose as the sampled pose from the start of the clip.
+	//	Result is the additive base pose.
+	//	When adding poses, there is now a reference for how much each bone has been transformed in this clip, for any time.
+	clip.Sample(result, clip.GetStartTime());
+
+	return result;
+}
+
+void BlendController::Add(Pose& output, Pose& inPose, Pose& addPose, Pose& additiveBasePose, int blendRoot)
+{
+	
+	unsigned int numJoints = addPose.GetSize();
+
+	for (int i = 0; i < numJoints; ++i)
+	{
+		if (blendRoot >= 0 && !IsInHierarchy(addPose, blendRoot, i))
+		{
+			continue;
+		}
+
+		Transform input = inPose.GetLocalTransform(i);
+
+		Transform additive = addPose.GetLocalTransform(i);
+
+		Transform additiveBase = additiveBasePose.GetLocalTransform(i);
+
+		//result pose = input pose + (additive pose = additive base pose)
+		Transform result(input.position + (additive.position - additiveBase.position), 
+			Normalised(input.rotation * (Inverse(additiveBase.rotation) * additive.rotation)), 
+			input.scale + (additive.scale - additiveBase.scale));
+		output.SetLocalTransform(i, result);
+		
+	}
+}
--- Source/BlendController.h
+++ Source/BlendController.h
@@ -10,6 +10,8 @@ class BlendController
 public:
 	static bool IsInHierarchy(Pose& pose, unsigned int parent, unsigned int search);
 	static void Blend(Pose& output, Pose& a, Pose& b, float t, int blendRoot);
+	Pose MakeAdditivePose(Skeleton& skeleton, Clip& clip);
+	void Add(Pose& output, Pose& inPose, Pose& addPose, Pose& additiveBasePose, int blendRoot);
 
 private:
 	BlendController();
